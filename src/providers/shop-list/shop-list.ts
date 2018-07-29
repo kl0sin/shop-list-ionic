30645913ed1058b2a171c1a18bab1a869141c1e2
Mateusz Kłosiński <mkklosin@gmail.com>
2018-07-29 13:57:38 +0200

    Data structure changed

--- src/providers/shop-list/shop-list.ts
+++ src/providers/shop-list/shop-list.ts
@@ -1,18 +1,68 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-
 @Injectable()
 export class ShopListProvider {
 
-  shopLists: Array<{listName: string, createDate: string, complete: boolean, items: number}>;
+  shopLists: Array<{listName: string,
+    createDate: string,
+    complete: boolean,
+    items: number,
+    itemsArr: Array<{
+      itemName: string,
+      complete: boolean }>
+  }>;
 
   constructor(public http: HttpClient) {
     this.shopLists = [
-      { listName: 'Example list 1', createDate: '10/10/18', complete: false, items: 10 },
-      { listName: 'Example list 2', createDate: '10/10/18', complete: false, items: 4 },
-      { listName: 'Example list 3', createDate: '10/10/18', complete: false, items: 15 },
-      { listName: 'Example list 4', createDate: '10/10/18', complete: true, items: 7 },
+      {
+        listName: 'Example list 1',
+        createDate: '10/10/18',
+        complete: false,
+        items: 10,
+        itemsArr: [
+          {
+            itemName: 'Apples',
+            complete: false,
+          }
+        ]
+      },
+      {
+        listName: 'Example list 2',
+        createDate: '10/10/18',
+        complete: false,
+        items: 4,
+        itemsArr: [
+          {
+            itemName: 'Apples',
+            complete: false,
+          }
+        ]
+      },
+      {
+        listName: 'Example list 3',
+        createDate: '10/10/18',
+        complete: false,
+        items: 15,
+        itemsArr: [
+          {
+            itemName: 'Apples',
+            complete: false,
+          }
+        ]
+      },
+      {
+        listName: 'Example list 4',
+        createDate: '10/10/18',
+        complete: true,
+        items: 7,
+        itemsArr: [
+          {
+            itemName: 'Apples',
+            complete: false,
+          }
+        ]
+      },
     ];
   }
   getShopLists() {
@@ -31,6 +81,12 @@ export class ShopListProvider {
         createDate: actualDate,
         complete: false,
         items: 0,
+        itemsArr: [
+          {
+            itemName: 'Apples',
+            complete: false,
+          }
+        ]
       });
     }
 }
